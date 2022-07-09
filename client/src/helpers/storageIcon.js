3fe021015e036da56ab716df003c937ef3867cee
Juanma <arenasrinconj@gmail.com>
2022-07-08 21:52:39 -0500

    bug was solved

--- client/src/helpers/storageIcon.js
+++ client/src/helpers/storageIcon.js
@@ -3,7 +3,7 @@ import { MdOutlinePhoneIphone } from "react-icons/md";
 import { FcGoogle } from "react-icons/fc";
 
 export const storageIcons = storageLocation => {
-    if(storageLocation === 'SIM') return FaSimCard
+    if(storageLocation === 'SIM 1') return FaSimCard
     if(storageLocation === 'Phone') return MdOutlinePhoneIphone
     if(storageLocation === 'Google') return FcGoogle
 }
--- server/routes/contacts.route.js
+++ server/routes/contacts.route.js
@@ -3,9 +3,6 @@ const userExtractor = require('../middlewares/userExtractor.js')
 
 const { postContact, getAllContacts, getOneContact, deleteContact, putContact } = require('../controllers/contacts.js')
 
-//The path is relative to the path at he index
-//the path at the index is: /api/users
-//So this path is according to the last one
 contactsRouter.get('/', getAllContacts)
 
 contactsRouter.get('/:id', getOneContact)
